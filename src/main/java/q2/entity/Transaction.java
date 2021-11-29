dcc03457565e2d90e9edc3b353d156bc09b7fd93
m-r-ebrahimi <emohammadreza568@gmail.com>
2021-11-29 23:39:12 +0330

    complete backend.

--- src/main/java/q2/entity/Transaction.java
+++ src/main/java/q2/entity/Transaction.java
@@ -1,6 +1,7 @@
 package q2.entity;
 
 import q2.entity.base.BaseEntity;
+import q2.entity.base.BaseEntityInterface;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
---
+++ src/main/java/q2/front/Menu.java
@@ -0,0 +1,7 @@
+package q2.front;
+
+public class Menu {
+    public static void menu(){
+
+    }
+}
